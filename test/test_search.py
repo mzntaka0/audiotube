b1bbfe15d7781a30683c24b349bb40c76de4d1b0
mzntaka0 <takao.mizuno110@gmail.com>
2018-12-14 01:56:32 +0900

    tiny modification

--- test/test_search.py
+++ test/test_search.py
@@ -13,8 +13,6 @@ class TestSearch(unittest.TestCase):
 
     def test_fetch_result_ids(self):
         search = audiotube.Search()
-        actual = tashizan(value1, value2)
-        self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':
