09904768b78cee63b40bbcba1f066df581282217
Takao Mizuno <takao.mizuno110@gmail.com>
2019-09-10 20:32:17 +0900

    modified setup.py

--- audiotube/version.py
+++ audiotube/version.py
@@ -3,7 +3,7 @@
 __version__ = '0.1.0'
 __title__ = 'audiotube'
 __description__ = "Easily can get audio and 'standard' captions from YouTube"
-__url__ = ''
+__url__ = 'https://github.com/mzntaka0/audiotube'
 __author__ = 'Takao Mizuno'
-__author_email__ = ''
+__author_email__ = 'takao.mizuno110@gmail.com'
 __license__ = 'Apache Software License'
