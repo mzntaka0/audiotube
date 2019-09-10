fa3ea449857a24c18db32faf3f7c208b0e9b1a80
Takao Mizuno <takao.mizuno110@gmail.com>
2019-09-10 14:00:06 +0900

    modified youtube.py and test

--- audiotube/__init__.py
+++ audiotube/__init__.py
@@ -1,7 +1,10 @@
 from audiotube.youtube import YouTube
 from audiotube.search import Search
+from audiotube.objects import YouTubeAudio, YouTubeAudios
 
 __all__ = [
     'YouTube',
-    'Search'
+    'Search',
+    'YouTubeAudio',
+    'YouTubeAudios'
 ]
