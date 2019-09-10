d82cdfe64f68f6b74bf21b9e695ec3ed215fad32
Takao Mizuno <takao.mizuno110@gmail.com>
2019-09-10 13:45:16 +0900

    modified test

---
+++ test/test_youtube.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+import unittest
+
+import audiotube
+from dotenv import load_dotenv
+load_dotenv()
+
+
+class TestYouTube(unittest.TestCase):
+
+    def test_fetch_result_ids(self):
+        youtube = audiotube.YouTube()
+        results = youtube.search('python')
+        assert isinstance(results, list)
+        assert isinstance(results[0], audiotube.objects.YouTubeAudio)
+
+
+if __name__ == '__main__':
+    unittest.main()
